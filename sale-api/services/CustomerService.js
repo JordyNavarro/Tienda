081d8891a0ef04c9dbe0bcbd1080f7f4e6cfa03c
Alvaro1560 <johao_elunico@hotmail.com>
2024-05-14 23:24:05 -0500

    apis subidos a docker funcionando

--- sale-api/services/CustomerService.js
+++ sale-api/services/CustomerService.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 module.exports={
 
     get:async function(dni){
-        const {data} = await axios.get("http://person_api:8080/customers/"+dni);
+        const {data} = await axios.get("http://customer_api:8080/customers/"+dni);
         return data[0]; 
     }
 }
